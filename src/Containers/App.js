2f94f98c50ca48de7144624fc2f42926e83769c0
Shezray Husain <64660355+shezray@users.noreply.github.com>
2020-06-11 23:48:00 -0500

    error correction

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../Components/CardList';
 import SearchBox from '../Components/SearchBox';
 import Scroll from '../Components/Scroll';
+import ErrorBoundary from '../Components/ErrorBoundary';
 
 
 class App extends Component {
@@ -36,7 +37,9 @@ render() {
 	       <h1>RoboSquad</h1>
 	       <SearchBox searchChange={this.onSearchChange} />
          <Scroll>
-	         <CardList robots = {filteredRobots} />
+           <ErrorBoundary>
+	           <CardList robots = {filteredRobots} />
+           </ErrorBoundary>
           </Scroll>
       </div>
     );   
