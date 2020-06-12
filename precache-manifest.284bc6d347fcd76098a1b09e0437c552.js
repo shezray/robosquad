2ec50f05021d01dd181f55ecb7c8602c9c49331e
Shezray Husain <64660355+shezray@users.noreply.github.com>
2020-06-12 00:01:09 -0500

    Updates

---
+++ precache-manifest.284bc6d347fcd76098a1b09e0437c552.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9880fa45f744804f616d2c49ed0b8cc0",
+    "url": "/robosquad/index.html"
+  },
+  {
+    "revision": "10357f1bd46e17d1fcf0",
+    "url": "/robosquad/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0f25a3d788626a9e29d9",
+    "url": "/robosquad/static/css/main.7248cea4.chunk.css"
+  },
+  {
+    "revision": "10357f1bd46e17d1fcf0",
+    "url": "/robosquad/static/js/2.9a8e4407.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robosquad/static/js/2.9a8e4407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f25a3d788626a9e29d9",
+    "url": "/robosquad/static/js/main.084b65f7.chunk.js"
+  },
+  {
+    "revision": "9adc57915c8f81de4d90",
+    "url": "/robosquad/static/js/runtime-main.11fe8f53.js"
+  }
+]);
